9d80086d46c53742c0834a6cc3d2295f6a93976a
ChappyDev <danjtchapman@gmail.com>
2024-09-25 17:51:25 -0600

    Add gitignore and Shape.java

---
+++ app/src/main/java/com/example/quokka/Shape.java
@@ -0,0 +1,11 @@
+package com.example.quokka;
+
+public abstract class Shape {
+    private Integer x;
+    private Integer y;
+
+    public Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
