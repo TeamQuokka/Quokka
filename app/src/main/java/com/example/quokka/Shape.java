708521a00c1a96a50c82d78d25638bdc3f91bcb9
Daniel Chapman <99625418+Chappydev@users.noreply.github.com>
2024-09-25 18:42:33 -0600

    Update Shape.java

--- app/src/main/java/com/example/quokka/Shape.java
+++ app/src/main/java/com/example/quokka/Shape.java
@@ -3,6 +3,7 @@ package com.example.quokka;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    private String color = "magenta";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
