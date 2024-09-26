8efa0827db8a8b212197ee79969c1a81e5a14ec3
soaibanur <nursoaiba@gmail.com>
2024-09-25 19:11:12 -0600

    added star color

--- app/src/main/java/com/example/quokka/Shape.java
+++ app/src/main/java/com/example/quokka/Shape.java
@@ -1,22 +1,15 @@
 package com.example.quokka;
 
+import android.graphics.Color;
+
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    private String color = "magenta";
+    private String color;
 
-    public Shape(Integer x, Integer y) {
+    public Shape(Integer x, Integer y, Color color) {
         this.x = x;
         this.y = y;
+        this.color = "green";
     }
-}
-
-public abstract class Square extends Shape{
-    private int side;
-
-    public Square(int side){
-        this.side = side;
-    }
-
-
 }
--- app/src/main/java/com/example/quokka/Star.java
+++ app/src/main/java/com/example/quokka/Star.java
@@ -3,5 +3,6 @@ package com.example.quokka;
 public class Star extends Shape{
     public Star(Integer x, Integer y) {
         super(x, y);
+
     }
 }
