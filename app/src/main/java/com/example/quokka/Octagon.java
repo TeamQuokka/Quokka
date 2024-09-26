18fd07bb343f5001e437d0cd03712a6697602b63
Simon Nakatsu <180454858+saimonnk@users.noreply.github.com>
2024-09-25 18:38:50 -0600

    add color simon branch

--- app/src/main/java/com/example/quokka/Octagon.java
+++ app/src/main/java/com/example/quokka/Octagon.java
@@ -1,7 +1,6 @@
 package com.example.quokka;
 // Simon's commit
 public class Octagon extends Shape{
-    String color = "cyan";
     public Octagon(Integer x, Integer y) {
         super(x, y);
 
--- app/src/main/java/com/example/quokka/Shape.java
+++ app/src/main/java/com/example/quokka/Shape.java
@@ -3,9 +3,10 @@ package com.example.quokka;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-
+    private String color;
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
+        this.color = "cyan";
     }
 }
