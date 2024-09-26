1a9e63cdcad1f95eae6e442ea878bceb2ef24c05
ChappyDev <danjtchapman@gmail.com>
2024-09-25 18:20:41 -0600

    Add color 'only on my branch tho'

--- app/src/main/java/com/example/quokka/Shape.java
+++ app/src/main/java/com/example/quokka/Shape.java
@@ -3,6 +3,7 @@ package com.example.quokka;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    private String color = "blue";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
