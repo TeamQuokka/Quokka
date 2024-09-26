04d13209aec501f7624e078ba62b16ba19baf877
Myla <myambao@ualberta.ca>
2024-09-25 19:05:40 -0600

    square

--- app/src/main/java/com/example/quokka/Shape.java
+++ app/src/main/java/com/example/quokka/Shape.java
@@ -3,26 +3,12 @@ package com.example.quokka;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-<<<<<<< HEAD
-   // private String color;
-=======
     private String color = "magenta";
->>>>>>> origin
 
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
-        this.color = "pink";
-
     }
 }
 
-public abstract class Square extends Shape{
-    private int side;
 
-    public Square(int side){
-        this.side = side;
-    }
-
-
-}
